648bda0588248e3fb9e9af912f887736fe1f436f
Damhuis <ldamhuis@hotmail.com>
2019-06-02 21:27:18 +0200

    added multiple sessions functionality

--- src/main/java/WebSockets/WebSocketEndpoint.java
+++ src/main/java/WebSockets/WebSocketEndpoint.java
@@ -6,6 +6,10 @@
 package WebSockets;
 
 import com.mycompany.travelpoint.domain.Comment;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.naming.InitialContext;
@@ -20,14 +24,14 @@ import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
-
+import javax.websocket.server.PathParam;
 
 /**
  *
  * @author Damhuis
  */
 @ServerEndpoint(
-        value = "/comments",
+        value = "/comments/{id}/id",
         encoders = GsonEncoder.class,
         decoders = GsonDecoder.class,
         configurator = SessionProvider.class
@@ -36,11 +40,14 @@ public class WebSocketEndpoint {
 
     private static final Logger LOG = Logger.getLogger(WebSocketEndpoint.class.getName());
 
+    private static final Map<Long, List<Session>> _sessions = new HashMap<>();
+
     private HttpSession httpSession;
 
     private Session session;
-    
+
     private static final CommentBean COMMENT_BEAN;
+
     static {
         final String name = "java:global/TravelPoint/CommentBean";
         try {
@@ -52,18 +59,28 @@ public class WebSocketEndpoint {
     }
 
     @OnOpen
-    public void onOpen(EndpointConfig endpointConfig, Session session) {
+    public void onOpen(@PathParam("id") long id, EndpointConfig endpointConfig, Session session) {
+        if (!_sessions.containsKey(id)) {
+            List<Session> sessions = new ArrayList<>();
+            sessions.add(session);
+            _sessions.put(id, sessions);
+        }
         this.httpSession = SessionProvider.provide(endpointConfig);
-        this.session = session;
+        //this.session = session;
         LOG.log(Level.INFO, "onOpen: endpointConfig: {0}, session: {1}", new Object[]{endpointConfig, session});
     }
 
     @OnMessage
     public void onMessage(Session session, Comment comment) throws EncodeException {
         LOG.log(Level.INFO, "received message with text: {0}", comment.getCommentId());
-        for(int i = 0; i<4;i++){
-            comment.setCommentId(i);
-            COMMENT_BEAN.send(session, comment);}
+        for (List<Session> sessions : _sessions.values()) {
+            for (Session s : sessions) {
+                for (int i = 0; i < 4; i++) {
+                    comment.setCommentId(i);
+                    COMMENT_BEAN.send(s, comment);
+                }
+            }
+        }
     }
 
     @OnClose
